55e5b8c74bcd4beb8f179d7981fa1aed91b82033
Elias Hezron <eliashezron@Eliass-MacBook-Pro.local>
2023-03-20 19:38:29 +0300

    add

--- client/src/components/Elements/ClientSlider.js
+++ client/src/components/Elements/ClientSlider.js
@@ -1,13 +1,13 @@
-import React from "react";
-import Slider from "react-slick";
-import styled from "styled-components";
+import React from "react"
+import Slider from "react-slick"
+import styled from "styled-components"
 // Assets
-import ClientLogo01 from "../../assets/img/clients/logo01.svg";
-import ClientLogo02 from "../../assets/img/clients/logo02.svg";
-import ClientLogo03 from "../../assets/img/clients/logo03.svg";
-import ClientLogo04 from "../../assets/img/clients/logo04.svg";
-import ClientLogo05 from "../../assets/img/clients/logo05.svg";
-import ClientLogo06 from "../../assets/img/clients/logo06.svg";
+import ClientLogo01 from "../../assets/img/clients/logo01.svg"
+import ClientLogo02 from "../../assets/img/clients/logo02.svg"
+import ClientLogo03 from "../../assets/img/clients/logo03.svg"
+import ClientLogo04 from "../../assets/img/clients/logo04.svg"
+import ClientLogo05 from "../../assets/img/clients/logo05.svg"
+import ClientLogo06 from "../../assets/img/clients/logo06.svg"
 
 export default function ClientSlider() {
   const settings = {
@@ -40,43 +40,44 @@ export default function ClientSlider() {
         },
       },
     ],
-  };
+  }
   return (
     <div>
+      <h1>built with</h1>
       <Slider {...settings}>
-        <LogoWrapper className="flexCenter">
-          <ImgStyle src={ClientLogo01} alt="client logo" />
+        <LogoWrapper className='flexCenter'>
+          <ImgStyle src={ClientLogo01} alt='client logo' />
         </LogoWrapper>
-        <LogoWrapper className="flexCenter">
-          <ImgStyle src={ClientLogo02} alt="client logo" />
+        <LogoWrapper className='flexCenter'>
+          <ImgStyle src={ClientLogo02} alt='client logo' />
         </LogoWrapper>
-        <LogoWrapper className="flexCenter">
-          <ImgStyle src={ClientLogo03} alt="client logo" />
+        <LogoWrapper className='flexCenter'>
+          <ImgStyle src={ClientLogo03} alt='client logo' />
         </LogoWrapper>
-        <LogoWrapper className="flexCenter">
-          <ImgStyle src={ClientLogo04} alt="client logo" />
+        <LogoWrapper className='flexCenter'>
+          <ImgStyle src={ClientLogo04} alt='client logo' />
         </LogoWrapper>
-        <LogoWrapper className="flexCenter">
-          <ImgStyle src={ClientLogo05} alt="client logo" />
+        <LogoWrapper className='flexCenter'>
+          <ImgStyle src={ClientLogo05} alt='client logo' />
         </LogoWrapper>
-        <LogoWrapper className="flexCenter">
-          <ImgStyle src={ClientLogo06} alt="client logo" />
+        <LogoWrapper className='flexCenter'>
+          <ImgStyle src={ClientLogo06} alt='client logo' />
         </LogoWrapper>
-        <LogoWrapper className="flexCenter">
-          <ImgStyle src={ClientLogo03} alt="client logo" />
+        <LogoWrapper className='flexCenter'>
+          <ImgStyle src={ClientLogo03} alt='client logo' />
         </LogoWrapper>
-        <LogoWrapper className="flexCenter">
-          <ImgStyle src={ClientLogo04} alt="client logo" />
+        <LogoWrapper className='flexCenter'>
+          <ImgStyle src={ClientLogo04} alt='client logo' />
         </LogoWrapper>
-        <LogoWrapper className="flexCenter">
-          <ImgStyle src={ClientLogo01} alt="client logo" />
+        <LogoWrapper className='flexCenter'>
+          <ImgStyle src={ClientLogo01} alt='client logo' />
         </LogoWrapper>
-        <LogoWrapper className="flexCenter">
-          <ImgStyle src={ClientLogo02} alt="client logo" />
+        <LogoWrapper className='flexCenter'>
+          <ImgStyle src={ClientLogo02} alt='client logo' />
         </LogoWrapper>
       </Slider>
     </div>
-  );
+  )
 }
 
 const LogoWrapper = styled.div`
@@ -87,9 +88,9 @@ const LogoWrapper = styled.div`
     outline: none;
     border: 0px;
   }
-`;
+`
 const ImgStyle = styled.img`
   width: 100%;
   height: 100%;
   padding: 10%;
-`;
+`
