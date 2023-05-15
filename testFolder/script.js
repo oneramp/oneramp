2a007b1f89ab9cdec2c673391472b3e2c34c4265
Jovan Mwesigwa <jovanmwesigwa79@gmail.com>
2023-05-15 03:07:58 +0300

    Added node-fetch route

--- testFolder/script.js
+++ testFolder/script.js
@@ -1,24 +1,32 @@
 // Import necessary modules
-const OneRamp = require("./node_modules/oneramp/dist/index.js").default
-const { ethers } = require("ethers")
-require("dotenv").config()
+
+// const OneRamp = require("./node_modules/oneramp/dist/index.js").default
+const OneRamp = require("oneramp").default;
+const { ethers } = require("ethers");
+require("dotenv").config();
 
 // Load private key from environment variables
-const { PRIVATEKEY: testPrivateKey } = process.env
+const { PRIVATEKEY: testPrivateKey } = process.env;
 
 // Create an ethers provider that connects to the Alfajores testnet
 const provider = new ethers.providers.JsonRpcProvider(
   "https://alfajores-forno.celo-testnet.org"
-)
+);
 
 // Create a wallet using the private key and the provider
-const wallet = new ethers.Wallet(testPrivateKey, provider)
+const wallet = new ethers.Wallet(testPrivateKey, provider);
 
-const clientPub = "RMPPUBK-cacbc4ef3f9903a3429b-X"
-const secretKey = "RMPSEC-a2fd9f528ef158d4f7e8b55741f9ce34e9bb6892-X"
+const clientPub = "RMPPUBK-cacbc4ef3f9903a3429b-X";
+const secretKey = "RMPSEC-a2fd9f528ef158d4f7e8b55741f9ce34e9bb6892-X";
 
 // Create a OneRamp instance, passing the network name, the provider, and the wallet to its constructor
-const oneRamp = new OneRamp("alfajores", clientPub, secretKey, provider, wallet)
+const oneRamp = new OneRamp(
+  "alfajores",
+  clientPub,
+  secretKey,
+  provider,
+  wallet
+);
 
 async function test() {
   try {
@@ -26,14 +34,14 @@ async function test() {
     const tx = await oneRamp.deposit(
       "0xc0EBB770F2c9CA7eD0dDeBa58Af101695Cf1BDc1",
       34500000000
-    )
+    );
     // If successful, log the transaction
-    console.log(tx)
+    console.log(tx);
   } catch (error) {
     // If an error occurs, log it
-    console.error("Error depositing:", error)
+    console.error("Error depositing:", error);
   }
 }
 
 // Run the test function
-test()
+test();
