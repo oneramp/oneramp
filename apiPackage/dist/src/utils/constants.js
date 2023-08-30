0ec7b0abde2b6920511a6f014bbd0ac4a79a8657
Jovan Mwesigwa <jovanmwesigwa79@gmail.com>
2023-08-30 14:32:26 +0300

    Removed the dist

--- apiPackage/dist/src/utils/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// const apiUrl = "https://oneramp-production.up.railway.app/api"
-const apiUrl = "http://localhost:4000/api";
-exports.default = apiUrl;
--- apiPackage/dist/types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EnviromentE = void 0;
-var EnviromentE;
-(function (EnviromentE) {
-    EnviromentE[EnviromentE["DEV"] = 0] = "DEV";
-    EnviromentE[EnviromentE["LIV"] = 1] = "LIV";
-})(EnviromentE = exports.EnviromentE || (exports.EnviromentE = {}));
