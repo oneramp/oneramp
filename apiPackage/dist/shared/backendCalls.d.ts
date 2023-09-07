ade80bb785a9ea0654d9ca695dbf3c38888b8ecd
Jovan Mwesigwa <jovanmwesigwa79@gmail.com>
2023-09-07 14:50:05 +0300

    removed dist

--- apiPackage/dist/shared/backendCalls.d.ts
+++
@@ -1,4 +0,0 @@
-import { CredentialsI, KYCFormI, KYCStatusI, UserCreds } from "../types";
-export declare const getStoreAuthCreds: (clientId: string, secret: string) => Promise<any>;
-export declare const getStoreKYCStatus: (storeId: string, appCreds: UserCreds) => Promise<KYCStatusI>;
-export declare const createStoreUserKYC: (data: KYCFormI, credentials: CredentialsI) => Promise<KYCFormI>;
